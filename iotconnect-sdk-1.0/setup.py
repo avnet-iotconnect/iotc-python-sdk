418800ee32fb2dfcaff5d5faa031cfb04e398533
cgraflund-avnet <connor.graflund@avnet.com>
2023-11-17 10:51:58 -0700

    Removed all references to depreciated jsonlib library. Updated maximum python to 3.10

--- iotconnect-sdk-1.0/setup.py
+++ iotconnect-sdk-1.0/setup.py
@@ -6,28 +6,26 @@ packages_requires=[]
 
 if 'win' in sys.platform:
     if sys.version_info >= (3, 5):
-        packages_requires=["paho-mqtt","ntplib","pypiwin32","jsonlib-python3"]
+        packages_requires=["paho-mqtt","ntplib","pypiwin32"]
     else:
         packages_requires=[]
         os.system('pip install paho-mqtt')
         os.system('pip install ntplib')
         os.system('pip install pypiwin32')
-        #os.system('pip install jsonlib')
 
 
 elif 'linux' in sys.platform :
     if sys.version_info >= (3, 5):
-        packages_requires=["paho-mqtt","ntplib","jsonlib-python3"]
+        packages_requires=["paho-mqtt","ntplib"]
     else:
         packages_requires=[]
         os.system('pip install paho-mqtt')
         os.system('pip install ntplib')
-        os.system('pip install jsonlib')
  
 setup(
     name="iotconnect-sdk",
     version="1.0",
-    python_requires=">=2.7,>=3.5,<3.9",
+    python_requires=">=2.7,>=3.5,<=3.10",
     description='SDK for D2C and C2D communication',
     license="MIT",
     author='SOFTWEB SOLUTIONS<admin@softwebsolutions.com> (https://www.softwebsolutions.com)',
@@ -41,6 +39,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: MIT License",
         "Operating System :: OS Independent"
     ],
