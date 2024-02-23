2f02fd37cc2132ca2d3012c609725ae458691724
sw-iot-neeravparasar <neerav.parasar@softwebsolutions.com>
2024-02-23 14:10:54 +0530

    setup file update for python up to 3.12

--- iotconnect-sdk-3.0.5/setup.py
+++ iotconnect-sdk-3.0.5/setup.py
@@ -6,28 +6,28 @@ packages_requires=[]
 
 if 'win' in sys.platform:
     if sys.version_info >= (3, 6):
-        packages_requires=["paho-mqtt","ntplib","pypiwin32","jsonlib-python3"]
+        packages_requires=["paho-mqtt","ntplib","pypiwin32"]
     else:
         packages_requires=[]
         os.system('pip install paho-mqtt')
         os.system('pip install ntplib')
         os.system('pip install pypiwin32')
-        #os.system('pip install jsonlib')
+        
 
 
 elif 'linux' in sys.platform :
     if sys.version_info >= (3, 6):
-        packages_requires=["paho-mqtt","ntplib","jsonlib-python3"]
+        packages_requires=["paho-mqtt","ntplib"]
     else:
         packages_requires=[]
         os.system('pip install paho-mqtt')
         os.system('pip install ntplib')
-        # os.system('pip install jsonlib')
+        
  
 setup(
     name="iotconnect-sdk",
     version="3.0.5",
-    python_requires=">=2.7,>=3.6,<3.9",
+    python_requires=">=2.7,>=3.5,>=3.11,<=3.12",
     description='SDK for D2C and C2D communication',
     license="MIT",
     author='SOFTWEB SOLUTIONS<admin@softwebsolutions.com> (https://www.softwebsolutions.com)',
@@ -41,6 +41,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "License :: MIT License",
         "Operating System :: OS Independent"
     ],
